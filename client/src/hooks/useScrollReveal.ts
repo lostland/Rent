be3e1bc30b9fbf278c4ef2f4f15dd93522b689fb
lostland <kimcs71@gmail.com>
2025-09-28 05:16:41 +0000

    ...

--- client/src/hooks/useScrollReveal.ts
+++ client/src/hooks/useScrollReveal.ts
@@ -24,7 +24,7 @@ export function useScrollReveal(
     selector = "[data-animate]",
     threshold = 0.2,
     rootMargin = "0px",
-    once = true,
+    once = false,
     dependencies = [],
   }: UseScrollRevealOptions = {},
 ) {
